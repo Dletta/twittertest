81ef72b42d6a20bd40fa6304fc2d32b9c1639f67
Jachen Duschletta <jcduschletta@gmail.com>
2018-03-08 14:48:52 -0800

    fixed ipcMain reference error

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path')
 const url = require('url')
 var twitter = require('ntwitter');
