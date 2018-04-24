47ce436d4c0b354abbe5b8d5e85cfd8328ab57d3
Jachen Duschletta <jcduschletta@gmail.com>
2018-04-24 13:56:10 -0700

    need to implement graph database to show filtered data

--- view/index.js
+++ view/index.js
@@ -2,12 +2,27 @@ var {ipcRenderer} = require('electron')
 
 var Tweets = function () {
   this.tweets = []
-  this.add = function (item) {
-    if(this.tweets.length < 10) {
-      this.tweets.push(item)
+  this.filteredTweets = []
+  this.filtered = function(ev){
+    var string = document.getElementById('search').value
+    console.log(string);
+    if(string == "*" || ""){
+      this.filteredTweets = this.tweets
     } else {
-      this.tweets.shift()
-      this.tweets.push(item)
+      this.filteredTweets = this.tweets.filter((el) => {
+        return el.label.toLowerCase().indexOf(string.toLowerCase()) > -1
+      })
+    }
+  }
+  this.time = function(ev){
+    var string = document.getElementById('time').value
+    console.log(string);
+    if(string == "*" || ""){
+      this.filteredTweets = this.tweets
+    } else {
+      this.filteredTweets = this.tweets.filter((el) => {
+        return el.time[0].toLowerCase().indexOf(string.toLowerCase()) > -1
+      })
     }
   }
 }
@@ -27,7 +42,18 @@ ipcRenderer.on('clientLog', (event, msg) => {
   console.log(msg);
 })
 
+function compare(a, b) {
+  if(a.time.length>b.time.length){
+      return -1
+  } else if(b.time.length > a.time.length) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
 ipcRenderer.on('newTweet', (event, data) => {
+  data.sort(compare)
   twit.tweets = data
 })
 
