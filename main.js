5ba3bbf0305080af9f5b1aef57e39172a7edd51b
Jachen Duschletta <jcduschletta@gmail.com>
2018-04-26 10:39:58 -0700

    reset gun database

--- main.js
+++ main.js
@@ -61,25 +61,14 @@ ipcMain.on('ready', (event, msg) =>{
 })
 
 function getStream () {
-  /*twit.search('Seattle OR Seattle Museum', {}, function(err, data) {
-    console.log(err);
-    win.webContents.send('newTweet', data)
-  });*/
   twit.stream('statuses/filter', {'locations':'-122.670518,47.210218,-121.368207,48.181262'}, function(stream) {
     stream.on('error', function(err){
       console.log(`Got error: ${err}`);
     })
 
     stream.on('data', function (data) {
-      var data = filterData(data) /* removed columns that are not necessary for trending data and add our own timestamp */
-      Words.input.push(data)
-
-      /*
-      var data = JSON.stringify(data) + os.EOL
-      fs.appendFile('tweetsSeattle.txt', data, (err)=>{
-        if (err) throw err;
-      })
-      */
+      var data = filterData(data)
+      win.webContents.send('newTweet', data)
     });
   });
 }
@@ -90,87 +79,9 @@ ipcMain.on('getStream', (event, msg) =>{
 })
 
 
-/*
-* Example Tweet after Filter
-{"created_at":"Fri Mar 16 15:29:01 +0000 2018","text":"@TravisMayfield THAT SURECWASCA QUICK VACATION TRAVIS.✌","timestamp":"2018-03-16 7:29:01"}
-*/
 
 /* Utility Functions */
 
-/*
-* Object that receives text as input.
-* We call it's process function periodically to process one text item to words
-* Another process will sort words with timestamps. Input is the timestamp with text from tweet.
-*/
-var gWordFactory = function () {
-  this.input = []
-  this.process = function() {
-    if(this.input.length >= 1 ){
-        var data = this.input.shift()
-        var time = data["timestamp"]
-        var words = []
-        words = data["text"].split(" ")
-        if(words.length >= 1){
-          for(let i=0;i<words.length;i++){
-            var temp = {}
-            temp.label = words[i]
-            temp.time = time
-            Data.input.push(temp)
-          }
-        }
-    }
-  }
-}
-
-/* Output is {label: word, time: timestring} */
-
-var gDataFactory = function () {
-  this.input = []
-  this.words = []
-  this.process = function() {
-    if(this.input.length >= 1) {
-      var data = this.input.shift()
-      if(this.words.length >= 1){
-        if(this.words.some(e => e.label == data.label)){
-          this.words.forEach(elem => {
-            if(elem.label == data.label ){
-              elem.time.push(data.time)
-            }
-          })
-        } else {
-          var temp = new DataRecord(data.label)
-          temp.time.push(data.time)
-          this.words.push(temp)
-        }
-      } else {
-        var temp = new DataRecord(data.label)
-        temp.time.push(data.time)
-        this.words.push(temp)
-      }
-    }
-  win.webContents.send('newTweet', this.words)
-  }
-}
-
-var DataRecord = function(lab) {
-  this.label = lab
-  this.time = []
-}
-
-var Words = new gWordFactory()
-
-function processWords (){
-  Words.process()
-}
-
-var Data = new gDataFactory()
-
-function processData() {
-  Data.process()
-}
-
-setInterval(processWords, 1000)
-setInterval(processData, 1000)
 
 /*
 * Function that filters the data down to text, created_at and adds our own timestamp
@@ -178,10 +89,15 @@ setInterval(processData, 1000)
 
 function filterData (data) {
   var temp = {}
-  temp["created_at"] = data["created_at"]
-  temp["timestamp"] = new Date().toString()
+  temp["location"] = data["place"]
+  temp["coordinates"] = data["coordinates"]
+  temp["timestamp"] = data["timestamp_ms"]
   temp["text"] = data["text"]
   return temp
 }
 
-/* Output: {"created_at":string, "timestamp":timestring, "text":tweetText}*/
+/* Output: {
+*"location":object,
+*"coordinates": null or array of coordinates,
+*"timestamp":ms since 1968,
+*"text":tweetText}*/
